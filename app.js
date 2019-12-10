733da52b95d09e36e96bba3fc45ca89b7e1e1717
tayyabaziz <tayyabaziz5@gmail.com>
2019-12-10 14:48:01 +0500

    Update

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 const socket = require('./socket')
 
-const app = express()
-
-const PORT = process.env.PORT || 3000
-
 if (process.env.NODE_ENV !== "production") {
   dotenv.config()
 }
+
+const app = express()
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.json({ status: 200, message: 'Service is OK.' })
